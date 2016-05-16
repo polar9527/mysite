974cdc01f17b917a19a751c7c136a3299e0cee6a
polar9527 <polar9527@163.com>
2016-05-16 10:38:33 +0800

    Retrieve posts by similarity
    	• Retrieve all tags for the current post.
    	• Get all posts that are tagged with any of those tags.
    	• Exclude the current post from that list to avoid recommending th
    	• Order the results by the number of tags shared with the current p
    	• In case of two or more posts with the same number of tags, recommost recent post.
    	• Limit the query to the number of posts we want to recommend

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.db.models import Count
 
 from taggit.models import Tag
 
@@ -35,15 +36,6 @@ def post_list(request, tag_slug=None):
     return render(request,'blog/post/list.html',{'posts': posts,'tag': tag})
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(
-            Post, slug=post,
-            status='published',
-            publish__year=year,
-            publish__month=month,
-            publish__day=day)
-    return render(request,'blog/post/detail.html',{'post': post})
-
-def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
                                    status='published',
                                    publish__year=year,
@@ -64,10 +56,18 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published\
+                        .filter(tags__in=post_tags_ids)\
+                        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                 .order_by('-same_tags','-publish')[:4]
     return render(request, 'blog/post/detail.html', {'post': post,
                                                      'comments': comments,
                                                      'comment_form': comment_form,
-                                                     'new_comment': new_comment})
+                                                     'new_comment': new_comment,
+                                                     'similar_posts': similar_posts})
 
 def post_share(request, post_id):
     # Retrieve post by id
